a96cc7e18596ed2c82f9908f357cb3ca8efb4c5a
siwen <623746556@qq.com>
2019-02-26 17:52:21 +0800

    上传

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
-
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',  //需要压缩的代码
+  entry: './src/bundle-7daa9f5fa7.js', 
+  entry: {
+    app: [
+      "babel-polyfill", 
+      "./src/bundle-7daa9f5fa7.js"
+    ],//需要压缩的代码
+  },
   output: {
     path: path.resolve(__dirname, 'dist'), //输出目录
-    filename: 'index.js'
+    filename: 'bundle-7daa9f5fa7.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: "babel-loader"
+      }
+    }]
   }
 };
