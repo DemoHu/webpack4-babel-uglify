0bc9daac4d49160af4f41759417616ca565543ef
siwen <623746556@qq.com>
2019-02-26 18:01:39 +0800

    添加一些说明

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/bundle-7daa9f5fa7.js', 
   entry: {
     app: [
       "babel-polyfill", 
-      "./src/bundle-7daa9f5fa7.js"
-    ],//需要压缩的代码
+      "./src/bundle-7daa9f5fa7.js" //需要压缩的js，目前只支持单个文件一个一个压缩 = =！
+    ],
   },
   output: {
     path: path.resolve(__dirname, 'dist'), //输出目录
-    filename: 'bundle-7daa9f5fa7.js'
+    filename: 'bundle-7daa9f5fa7.js' //输出文件名称，建议与源文件同名
   },
   module: {
     rules: [{
